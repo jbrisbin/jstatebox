9b7d7656a4dead4193fe610e7788f483289c300b
Jon Brisbin <jon@jbrisbin.com>
2011-11-01 16:56:51 -0500

    Tweaked README, added some documentation.

--- README.md
+++ README.md
@@ -57,3 +57,21 @@ The value of `state4` is now "Hello World!". It is the composition of all the op
 on all the stateboxes you merge. They are applied in order based on the system time. Operations
 added to a statebox within 1ms of another operation are still performed, but the order in which
 they are performed is undefined.
+
+### License
+
+JStatebox is Apache 2.0 licensed.
+
+    Copyright 2011 the original author or authors.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+          http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
--- src/main/java/jstatebox/core/Statebox.java
+++ src/main/java/jstatebox/core/Statebox.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package jstatebox.core;
 
 import java.io.Serializable;
@@ -13,6 +29,10 @@ import org.slf4j.LoggerFactory;
 import scala.Function1;
 
 /**
+ * A statebox implementation for the JVM. Understands Java, Groovy, and Scala natively.
+ * It allows asynchronous JVM applications to alter the state of objects without blocking
+ * and by resolving conflicts at read time, rather than write time.
+ *
  * @author Jon Brisbin <jon@jbrisbin.com>
  */
 public class Statebox<T> implements Serializable {
@@ -43,18 +63,39 @@ public class Statebox<T> implements Serializable {
     this.value = value;
   }
 
+  /**
+   * Create a new statebox, wrapping the given value.
+   *
+   * @param value
+   */
   public static <T> Statebox<T> create(T value) {
     return new Statebox<>(value);
   }
 
+  /**
+   * Retrieve the value originally set in this statebox.
+   *
+   * @return
+   */
   public T value() {
     return value;
   }
 
+  /**
+   * When this statebox was last modified or merged.
+   *
+   * @return
+   */
   public Long lastModified() {
     return lastModified;
   }
 
+  /**
+   * Remove any operations that are older than the given age.
+   *
+   * @param age
+   * @return
+   */
   public Statebox<T> expire(Long age) {
     List<StateboxOp> opsToRemove = new ArrayList<>();
     Long expiration = lastModified - age;
@@ -68,11 +109,27 @@ public class Statebox<T> implements Serializable {
     return this;
   }
 
+  /**
+   * Mutate the value of this statebox into a new statebox that is the result of call
+   * the operation and passing the current value.
+   *
+   * @param operation
+   * @param <Op>
+   * @return
+   */
   public <Op> Statebox<T> modify(Op operation) {
     ops.add(new StateboxOp(operation));
+    lastModified = System.currentTimeMillis();
+
     return new Statebox<>(invoke(operation, value()));
   }
 
+  /**
+   * Merge the operations of the given stateboxes into a single value based on timestamp.
+   *
+   * @param stateboxes
+   * @return
+   */
   @SuppressWarnings({"unchecked"})
   public Statebox<T> merge(Statebox... stateboxes) {
     for (Statebox st : stateboxes) {
@@ -130,53 +187,6 @@ public class Statebox<T> implements Serializable {
         ",\n\t ops=" + ops +
         ",\n\t lastModified=" + lastModified +
         "\n}";
-  }
-
-  private class OpRunner<T> implements Runnable {
-
-    private T obj;
-    private final Object op;
-
-    private OpRunner(Object op) {
-      this.op = op;
-    }
-
-    @SuppressWarnings({"unchecked"})
-    @Override public void run() {
-      if (op instanceof Operation) {
-        obj = ((Operation<T>) op).invoke(obj);
-        return;
-      }
-
-      if (IS_GROOVY_PRESENT) {
-        if (op instanceof Closure) {
-          obj = (T) ((Closure) op).call(obj);
-          return;
-        }
-      }
-
-      if (IS_SCALA_PRESENT) {
-        if (op instanceof Function1) {
-          obj = (T) ((Function1) op).apply(obj);
-          return;
-        }
-      }
-
-      if (op instanceof Runnable) {
-        ((Runnable) op).run();
-        return;
-      }
-
-      if (op instanceof Callable) {
-        try {
-          obj = ((Callable<T>) op).call();
-        } catch (Exception e) {
-          throw new IllegalStateException(e);
-        }
-        return;
-      }
-    }
-
   }
 
   private class StateboxOp implements Comparable<StateboxOp> {
